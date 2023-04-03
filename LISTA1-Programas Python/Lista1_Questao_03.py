d61720f2aa07a448bd7d524c1875172068f5898e
Isaias Felipe <126331149+Isaiasfelipe7@users.noreply.github.com>
2023-04-03 10:35:35 -0300

    Novo projeto

--- LISTA1-Programas Python/Lista1_Questao_03.py
+++ LISTA1-Programas Python/Lista1_Questao_03.py
@@ -1,5 +1,4 @@
-#Lista1_Questão 03. Escreva um programa para ler uma temperatura em graus Fahrenheit. Faça uma função chamada celsius para calcular e retornar 
-# o valor correspondente em graus Celsius.
+#Lista1_Questão 03. Escreva um programa para ler uma temperatura em graus Fahrenheit. Faça uma função chamada celsius para calcular e retornar o valor correspondente em graus Celsius.
 #      Fórmula: C = ((F-32)/9)*5
 
 def celsius(tmp):
