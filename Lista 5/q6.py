ee4a48ebefbc394aa47f936f41bf692127c8bbae
Isaias Felipe <126331149+Isaiasfelipe7@users.noreply.github.com>
2023-06-12 09:24:31 -0300

    Update q6.py

--- Lista 5/q6.py
+++ Lista 5/q6.py
@@ -1,3 +1,27 @@
 ''' Lista5_q6. Crie uma função chamada "contar_substrings" que receba uma string e uma substring
 como parâmetros e retorne a quantidade de ocorrências da substring na string.
 '''
+
+def contar_substrings(strin, subs):
+    if type(strin)!= str or type(subs) != str:
+        return Exception
+    
+    if len(strin) == 0 or len(subs) == 0:
+        return Exception
+
+    contador = 0
+    tamanho_substring = len(subs)
+    tamanho_string = len(strin)
+    
+    for i in range(tamanho_string - tamanho_substring + 1):
+        if strin[i:i+tamanho_substring] == subs:
+            contador += 1
+    
+    return contador
+
+assert contar_substrings(2, 'y') == Exception
+assert contar_substrings('x', 5) == Exception
+assert contar_substrings('', 'in') == Exception
+assert contar_substrings('banana', 'an') == 2
+
+print('Testes ok!')
